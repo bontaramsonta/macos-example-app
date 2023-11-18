59376d7eb8b97cab5c08ec1feb7fedf02ba0bf93
bontaramsonta <18cs113@gmail.com>
2023-11-18 20:28:41 +0530

    Initial Commit

---
+++ example app/example_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_appApp.swift
+//  example app
+//
+//  Created by bontaramsonta on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct example_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
